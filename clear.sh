51e7b6aef558f7934613d8d84b8a6e876f392525
zjx-immersion <jianxin_home@163.com>
2017-06-06 12:04:57 +0800

    [Jason]fix issue to execute script in pipeline 6

--- clear.sh
+++ clear.sh
@@ -1,11 +1,16 @@
 #!/bin/bash 
 docker ps -a | grep zhongjx/demonode | awk "{print \$1}"
 
-docker ps -a | grep zhongjx/demonode | awk "{print \$1}" | xargs -r docker rm -f
+# docker ps -a | grep zhongjx/demonode | awk "{print \$1}" | xargs -r docker rm -f
+docker ps -a | grep zhongjx/demonode | awk "{print \$1}" | xargs --no-run-if-empty docker rm -f
 
-docker images --filter=reference='*zhongjx/demonode:*' | awk "{print \$3}"
+# docker images --filter=reference='*zhongjx/demonode:*' | awk "{print \$3}"
 
-docker images --filter=reference='*zhongjx/demonode:*' | awk "{print \$3}" | xargs -r docker rmi -f
+# docker images --filter=reference='*zhongjx/demonode:*' | awk "{print \$3}" | xargs --no-run-if-empty docker rmi -f
+
+docker images | grep zhongjx/demonode| awk "{print \$3}" 
+
+docker images | grep zhongjx/demonode| awk "{print \$3}" | xargs --no-run-if-empty docker rmi -f
 
 
 
