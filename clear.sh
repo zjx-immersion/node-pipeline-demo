471ee9555f924969b04c5d36b043ae16316bdead
zjx-immersion <jianxin_home@163.com>
2017-06-06 11:52:18 +0800

    [Jason]fix issue to execute script in pipeline 5

--- clear.sh
+++ clear.sh
@@ -1,8 +1,14 @@
 #!/bin/bash 
 docker ps -a | grep zhongjx/demonode | awk "{print \$1}"
 
-docker ps -a | grep zhongjx/demonode | awk "{print \$1}" | xargs docker rm -f
+docker ps -a | grep zhongjx/demonode | awk "{print \$1}" | xargs -r docker rm -f
 
 docker images --filter=reference='*zhongjx/demonode:*' | awk "{print \$3}"
 
-docker images --filter=reference='*zhongjx/demonode:*' | awk "{print \$3}" | xargs docker rmi -f
+docker images --filter=reference='*zhongjx/demonode:*' | awk "{print \$3}" | xargs -r docker rmi -f
+
+
+
+# docker rm -f  $(docker ps -aq -f name='nodedemo')
+
+# docker rmi -f $(docker images -q  -f reference='zhongjx/demonode')
