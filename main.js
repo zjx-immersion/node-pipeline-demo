9b13ab1e3d46bb3912abec8a392eb7ecf3dd8b24
zjx-immersion <jianxin_home@163.com>
2017-06-13 14:22:59 +0800

    {jsaon}update for NHO

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n zjx");
+  response.end("Hello getintodevops.com\n NHO_Training");
 });
 
 // listen on localhost:8000
